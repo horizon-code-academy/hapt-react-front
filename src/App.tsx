cbc9b0230a6cb2237ee482db7550d2f1c3c25e56
Malek Boubakri <malekbouba@gmail.com>
2022-02-09 17:40:48 +0100

    init login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from "react";
 import "./App.css";
-import Info from "./components/Info";
-
-const u1 = {
-  firstName: "Malek",
-  lastName: "Boubakri",
-  birthDate: "12-07-1993",
-  address: "Trocadero, Sousse",
-};
-
-const f1 = {
-  label: "React",
-  nb_hour: 15,
-};
 
 function App() {
   return (
     <div className="container-fluid">
-      <h1>Hello, {u1.firstName}</h1>
-      <Info user={u1} subject={f1} />
+      <div className="row">
+        <div className="col-md-6 col-12 screen bg-dark "></div>
+        <div className="col-md-6 col-12 login text-center p-5 d-flex flex-column justify-content-center">
+          <h1>Login</h1>
+          <p>Please login to your account.</p>
+          <form className="form">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="row">
+              <div className="col dflex">
+                <div className="form-check justify-content-left">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexCheckDefault"
+                  >
+                    Remember me
+                  </label>
+                </div>
+              </div>
+              <div className="col justify-content-right">
+                <a href="#">Forget password</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
