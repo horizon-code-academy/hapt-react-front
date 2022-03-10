f736e93fd97e013686055507af82d00cc3d852f0
Malek Boubakri <malekbouba@gmail.com>
2022-03-10 17:49:59 +0100

    fix links

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import ForgetPassword from "./components/ForgetPassword";
 
 function App() {
-    return (
-        <Routes>
-            <Route path={"/"} element={<Login/>}/>
-            <Route path={"/dashboard"} element={<Dashboard/>}/>
-            <Route path={"/forget"} element={<ForgetPassword/>}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path={"/"} element={<Dashboard />} />
+      <Route path={"login"} element={<Login />} />
+      <Route path={"forget"} element={<ForgetPassword />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,82 +1,90 @@
-import {ChangeEvent, useState} from "react";
-import {FormattedMessage} from "react-intl";
-import {Button, Col, Container, Form, FormGroup, Input, Row,} from "reactstrap";
-import {Link} from "react-router-dom";
+import { ChangeEvent, useState } from "react";
+import { FormattedMessage } from "react-intl";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Row,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function Login() {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const changeEmail = (e: ChangeEvent<HTMLInputElement>) =>
-        setEmail(e.target.value);
+  const changeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
 
-    const changePassword = (e: ChangeEvent<HTMLInputElement>) =>
-        setPassword(e.target.value);
+  const changePassword = (e: ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
 
-    return (
-        <Container fluid>
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={6} className="screen bg-dark " />
+        <Col
+          xs={12}
+          md={6}
+          className="login text-center p-5 d-flex flex-column justify-content-center"
+        >
+          <FormattedMessage tagName="h1" id="login.title" />
+          <FormattedMessage tagName="p" id="login.subtitle" />
+          <Form>
+            <FormGroup floating className="mb-3">
+              <Input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                value={email}
+                onChange={changeEmail}
+              />
+              <FormattedMessage tagName="label" id="login.email" />
+            </FormGroup>
+            <FormGroup floating>
+              <Input
+                type="password"
+                id="floatingPassword"
+                placeholder="Password"
+                value={password}
+                onChange={changePassword}
+              />
+              <FormattedMessage tagName="label" id="login.pass" />
+            </FormGroup>
             <Row>
-                <Col xs={12} md={6} className="screen bg-dark "/>
-                <Col
-                    xs={12}
-                    md={6}
-                    className="login text-center p-5 d-flex flex-column justify-content-center"
-                >
-                    <FormattedMessage tagName="h1" id="login.title"/>
-                    <FormattedMessage tagName="p" id="login.subtitle"/>
-                    <Form>
-                        <FormGroup floating className="mb-3">
-                            <Input
-                                type="email"
-                                className="form-control"
-                                id="floatingInput"
-                                placeholder="name@example.com"
-                                value={email}
-                                onChange={changeEmail}
-                            />
-                            <FormattedMessage tagName="label" id="login.email"/>
-                        </FormGroup>
-                        <FormGroup floating>
-                            <Input
-                                type="password"
-                                id="floatingPassword"
-                                placeholder="Password"
-                                value={password}
-                                onChange={changePassword}
-                            />
-                            <FormattedMessage tagName="label" id="login.pass"/>
-                        </FormGroup>
-                        <Row>
-                            <Col className="d-flex">
-                                <FormGroup check inline className="text-left">
-                                    <input
-                                        className="form-check-input"
-                                        type="checkbox"
-                                        value=""
-                                        id="flexCheckDefault"
-                                    />
+              <Col className="d-flex">
+                <FormGroup check inline className="text-left">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
 
-                                    <FormattedMessage tagName="label" id="login.text"/>
-                                </FormGroup>
-                            </Col>
-                            <Col>
-                                <Link to={"forget"}>
-                                    <FormattedMessage id="login.forget"/>
-                                </Link>
-                            </Col>
-                        </Row>
-
-                        <Button tag={Link} to={"/dashboard"} type="button" color="dark" size="lg">
-                            <FormattedMessage id="login.btn"/>
-                        </Button>
-                    </Form>
-                    <footer className="text-center">
-                        <FormattedMessage id="login.copy"/>
-                    </footer>
-                </Col>
+                  <FormattedMessage tagName="label" id="login.text" />
+                </FormGroup>
+              </Col>
+              <Col>
+                <Link to="/forget">
+                  <FormattedMessage id="login.forget" />
+                </Link>
+              </Col>
             </Row>
-        </Container>
-    );
+
+            <Button tag={Link} to="/" type="button" color="dark" size="lg">
+              <FormattedMessage id="login.btn" />
+            </Button>
+          </Form>
+          <footer className="text-center">
+            <FormattedMessage id="login.copy" />
+          </footer>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
